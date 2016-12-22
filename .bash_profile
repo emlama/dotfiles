48af98f0fdab4d2aa66f770c458015df6c541af7
Matt Saforrian <msaforrian@quickbase.com>
2016-12-22 12:48:32 -0500

    Initial commit

---
+++ .bash_profile
@@ -0,0 +1,16 @@
+HISTSIZE=10000
+HISTFILESIZE=10000000
+HISTTIMEFORMAT='%F %T '
+
+alias serve='python -m SimpleHTTPServer 4000'
+
+export PACKAGE_DIRS="/Users/msaforrian/src/meteor-packages-local"
+export EDITOR=/usr/local/bin/subl
+export BLOCKSIZE=1k
+
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+
+export PS1="\w @ (\u) => "
+
+export HOMEBREW_GITHUB_API_TOKEN="bfd211e0defd3728ad3c542758a73c9e5e5bd3aa"
---
+++ install.sh
@@ -0,0 +1,26 @@
+# Install devtools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install CLI tools
+brew install yarn mas trash autojump hyper git wget nmap 
+npm i -g eslint hpm-cli stylelint
+hpm i hyperterm-material hyperterm-blink hyperterm-tab-icons hyperlinks
+
+# Install Apps via Cask
+brew cask install google-chrome sublime-text alfred istat-menus numi flux skyfonts transmission firefox vlc spotify axure-rp balsamiq-mockups cakebrew framer github-desktop
+
+# Signin to MAS
+mas signin matthew.forr@gmail.com
+
+# Install App Store apps
+mas install 824183456 411811718 587512244 824171161 926036361 405772121 623795237 889428659 1052074952 421879749 568494494 410628904 409789998 449589707 413965349 803453959 724408341 405904955 411643860 413857545 408981434 775737590 409183694 1173932628
+
+# create config files
+# echo 'source $HOME/dotfiles/.zshrc' > ~/.zshrc
+mv ~/.bash_profile ~/.bash_profile_old
+cp -f .bash_profile ~
+cp -f .gitignore ~
+cp -f .gitconfig ~
